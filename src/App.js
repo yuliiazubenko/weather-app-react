75d36c2f3c5537be4714bb0ad390adcab8c16ec2
yuliiazubenko <julia.zubenko6@gmail.com>
2022-09-14 16:53:05 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This App was coded by Yuliia Zubenko and is{" "}
-        <a
-          href="https://github.com/yuliiazubenko/weather-app-react"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This App was coded by Yuliia Zubenko and is{" "}
+          <a
+            href="https://github.com/yuliiazubenko/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

8ee695d2ae27a65a1ac03d6b02901e962db38729
yuliiazubenko <julia.zubenko6@gmail.com>
2022-09-14 15:51:33 +0300

    Removed some files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import "./index.css";
+import "./App.css";
 import React from "react";
-import Search from "./Search";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <Search />
     </div>
   );
 }

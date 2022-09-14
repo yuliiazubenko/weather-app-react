41438ca7b7dde23e8147a3e6ad471a6662260fea
yuliiazubenko <julia.zubenko6@gmail.com>
2022-09-14 16:00:00 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This App was coded by Yuliia Zubenko and is{" "}
+        <a
+          href="https://github.com/yuliiazubenko/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
